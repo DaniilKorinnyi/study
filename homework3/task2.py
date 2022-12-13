4691fa6baf41c04bf6231e1d8dd38d2fda1e3ee7
Коренной Даниил <dania.korinnoy@MacBook-Air-Korennoj.local>
2022-12-13 14:57:28 +0200

    Some changes in task2and3

--- homework3/task2.py
+++ homework3/task2.py
@@ -1,4 +1,4 @@
-your_age =input("Write your age: ")
-age_in_months = int(your_age) * 12
-print("Your age in months: ")
+your_age = 21
+age_in_months = your_age * 12
+print("My age in months: ")
 print(age_in_months)
--- homework3/task3.py
+++ homework3/task3.py
@@ -1,5 +1,5 @@
-my_name = input("Your name: ")
-age_in_years = input("Your age: ")
+my_name = ("Dania")
+age_in_years = str(21)
 my_age = ("My name is "+ my_name + ", I'm " + age_in_years + " years old.")
 print(my_age)
 
