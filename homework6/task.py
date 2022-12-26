e29e3cd7189d2bd9bc94c1c1ca716ab3daa38061
Коренной Даниил <dania.korinnoy@MacBook-Air-Korennoj.local>
2022-12-26 16:12:21 +0200

    Update homework6

--- homework6/task.py
+++ homework6/task.py
@@ -4,7 +4,7 @@ phone_book_list = [
     ["Alice", "+063672317"]
 ]
 phone_book = dict(phone_book_list)
-stats = len(phone_book)
+
 while True:
     print("What do you want to do: ")
     b = input()
@@ -12,19 +12,29 @@ while True:
     def add():
         print("Write name: ")
         name = input()
-        print("Write his/her number: ")
-        c = input()
-        phone_book[name] = c
-        print(phone_book)
+
+        if name in phone_book:
+            print("Such name has already exist.")
+        else:
+            print("Write his/her number: ")
+            c = input()
+            phone_book[name] = c
+            print(phone_book)
+    def delete():
+        print("Write name: ")
+        name = input()
+        if name in phone_book:
+            del phone_book[name]
+            print(phone_book)
+        elif phone_book.get(name) is None :
+            print(f"{name} doesn't exist.")
 
     if b == "add":
         add()
     elif b == "delete":
-        print("Write name: ")
-        name = input()
-        del phone_book[name]
-        print(phone_book)
+        delete()
     elif b == "stat":
+        stats = len(phone_book)
         print(f"You have {stats} contacts in your phonebook")
     elif b == "list":
         print(phone_book)
