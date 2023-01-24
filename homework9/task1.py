5161604955fc24112a9635010926cd2de99eda74
Коренной Даниил <dania.korinnoy@MacBook-Air-Korennoj.local>
2023-01-24 16:48:09 +0200

    Task1 is changed

--- homework9/task1.py
+++ homework9/task1.py
@@ -1,34 +1,61 @@
 import json
 
-phone_book_dict = {}
-try:
-    with open('telephonebook', 'r') as file:
-        phone_book_dict = json.load(file)
-except:
-    pass
+def load_phonebook(file_name):
+    try:
+        with open(file_name, 'r') as f:
+            phone_book = json.load(f)
+            return phone_book
+    except FileNotFoundError:
+        return {}
 
-def add(name, number):
-    phone_book_dict[name] = number
-    with open('telephonebook', 'w') as file:
-        json.dump(phone_book_dict, file)
+def save_phonebook(file_name, phone_book):
+    with open(file_name, 'w') as f:
+        json.dump(phone_book, f)
 
-def delete(name):
-    del phone_book_dict[name]
-    with open('telephonebook', 'w') as file:
-        json.dump(phone_book_dict, file)
+file_name = 'phone_book.json'
+phone_book = load_phonebook(file_name)
 
-def update(name, number):
-    phone_book_dict[name] = number
-    with open('telephonebook', 'w') as file:
-        json.dump(phone_book_dict, file)
 while True:
-    a = input("What do you want to do (add/delete): ")
-    if a == "add":
-        name = input("Write name: ")
-        number = input("Write number: ")
-        add(name, number)
-        print(phone_book_dict)
-    elif a == "delete":
-        name = input("Write name: ")
-        delete(name)
-        print(phone_book_dict)
+    print("What do you want to do: ")
+    b = input()
+
+    def add():
+        print("Write name: ")
+        name = input()
+
+        if name in phone_book:
+            print("Such name has already exist.")
+        else:
+            print("Write his/her number: ")
+            c = input()
+            phone_book[name] = c
+            save_phonebook(file_name, phone_book)
+            print(phone_book)
+    def delete():
+        print("Write name: ")
+        name = input()
+        if name in phone_book:
+            del phone_book[name]
+            save_phonebook(file_name, phone_book)
+            print(phone_book)
+        elif phone_book.get(name) is None :
+            print(f"{name} doesn't exist.")
+
+    if b == "add":
+        add()
+    elif b == "delete":
+        delete()
+    elif b == "stat":
+        stats = len(phone_book)
+        print(f"You have {stats} contacts in your phonebook")
+    elif b == "list":
+        print(phone_book)
+    elif b == "show":
+        print("Write name: ")
+        name = input()
+        if name in phone_book:
+            print(phone_book.get(name))
+        else:
+            print("There isn't such name in your phone book.")
+    else:
+        print("Command not found")
