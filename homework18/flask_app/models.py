c3b082befb8591744bb6522d883884121bd17c2a
dania.korinnoy <dania.korinnoy@gmail.com>
2023-04-12 23:30:21 +0300

    Some files have been added

---
+++ homework18/flask_app/models.py
@@ -0,0 +1,21 @@
+from flask_app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+
+class Purchase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    purchase_date = db.Column(db.DateTime, nullable=False)
